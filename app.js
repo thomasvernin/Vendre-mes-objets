a34734a0fa806bfa810b067706bdbdf084ae83a5
Thomas Vernin <tvernin49100@gmail.com>
2024-06-28 14:01:16 +0200

    Ajout de la fonctionnalité de modification des articles a vendre

--- app.js
+++ app.js
@@ -29,12 +29,25 @@ app.post('/api/stuff', (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
+  
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
-    app.use('/api/stuff', (req, res, next) => {
-        Thing.find()
-          .then(things => res.status(200).json(things))
-          .catch(error => res.status(400).json({ error }));
-        }); 
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+       .then(things => res.status(200).json(things))
+       .catch(error => res.status(400).json({ error }));
+  }); 
 
 
 module.exports = app;
