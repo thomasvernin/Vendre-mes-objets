a13158c50678bb490aa2bab22a3761fe90f87de3
Thomas Vernin <tvernin49100@gmail.com>
2024-06-28 13:45:12 +0200

    ajout de la fonctionnalité ajouter un objet

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose'); 
 const app = express();
 
+const Thing = require('./models/thing');
+
 
 mongoose.connect('mongodb+srv://tvernin49100:tvernin49100@thomasvernin.d5v4iay.mongodb.net/?retryWrites=true&w=majority&appName=Thomasvernin', 
   { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,33 +20,22 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Objet créé !'
+    delete req.body._id;
+    const thing = new Thing({
+      ...req.body
+    });
+    thing.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
   });
-});
 
-app.get('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
-});
+
+    app.use('/api/stuff', (req, res, next) => {
+        Thing.find()
+          .then(things => res.status(200).json(things))
+          .catch(error => res.status(400).json({ error }));
+        }); 
+
 
 module.exports = app;
 
