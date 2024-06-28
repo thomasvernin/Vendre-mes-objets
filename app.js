c7712cfe6c70824eaf752cdca8c5ae06ba23eff5
Thomas Vernin <tvernin49100@gmail.com>
2024-06-28 14:07:45 +0200

    Ajout de la fonctionnalité DELETE des articles

--- app.js
+++ app.js
@@ -29,13 +29,18 @@ app.post('/api/stuff', (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   });
 
-  
+
 app.put('/api/stuff/:id', (req, res, next) => {
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
   });
 
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
