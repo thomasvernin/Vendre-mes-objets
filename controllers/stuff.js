001e27e4b64e84b65addeaf52d054cf30dde2847
Thomas Vernin <tvernin49100@gmail.com>
2024-07-01 14:25:52 +0200

    modification de la route delete

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
     const thingObject = JSON.parse(req.body.thing);
@@ -53,20 +54,23 @@ exports.modifyThing = (req, res, next) => {
         });
  };
 
-exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
+ exports.deleteThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id})
+      .then(thing => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({message: 'Not authorized'});
+          } else {
+              const filename = thing.imageUrl.split('/images/')[1];
+              fs.unlink(`images/${filename}`, () => {
+                  Thing.deleteOne({_id: req.params.id})
+                      .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                      .catch(error => res.status(401).json({ error }));
+              });
+          }
+      })
+      .catch( error => {
+          res.status(500).json({ error });
       });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
 };
 
 exports.getAllStuff = (req, res, next) => {
